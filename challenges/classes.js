683962ad29f21baa69c7456a4a5d452d454c3e19
emmacodes19 <berdae@gmail.com>
2019-09-20 11:35:17 -0500

    js challenge

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,7 +1,70 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+
+class CuboidMaker {
+    constructor(attributes) {
+        this.newLength = attributes.length,
+            this.newWidth = attributes.width,
+            this.newHeight = attributes.height
+    }
+    
+    volume() {
+        return (this.length * this.width * this.height);
+    }
+    
+    surfaceArea() {
+        return 2 * (this.newLength * this.newWidth + this.newLength * this.newHeight + this.newWidth * this.newHeight);
+    }
+}
+
+
+
+
+const cuboid = new CuboidMaker( {
+    length: 4,
+    width: 5,
+    height: 5
+
+});
+
+
+
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker {
+    constructor(cubeAttributes){
+        super(cubeAttributes);
+        
+    }
+
+    volume_cube() {
+
+        return Math.pow(this.newLength, 3);
+
+    }
+
+    surfaceArea_cube() {
+        return 6(Math.pow(this.newLength, 2));
+    }
+}
+
+
+
+const cube = new CubeMaker ({
+    
+    length: 2,
+    width: 2,
+    height: 2
+    
+    
+
+});
+
+console.log(cube.volume_cube());
+console.loh(cube.surfaceArea_cube());
+
--- challenges/functions.js
+++ challenges/functions.js
@@ -1,4 +1,4 @@
-// ==== Callbacks ====  
+// / ==== Callbacks ====  
 
 /* Step 1: Create a higher-order function
   * Create a higher-order function named consume with 3 parameters: a, b and cb
@@ -6,19 +6,55 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
+function consume(a, b, cb) {
+    return cb (a, b);
+}
 
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+  
 */
+
+const add = function (a, b){
+    return  a + b;
+}
+
+  
+
+
+
+
+const multiply = function (a, b ) {
+    return a * b
+  }
+  
+
+
+  const greeting = function ( first_name, last_name) {
+      return (`Hello ${first_name} ${last_name}, nice to meet you!`);
+  }
+   
+
+
+
+// console.log(add(4,4,sum));
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+
+
+
+
+
+
+
+
 
 
 // ==== Closures ==== 
@@ -26,6 +62,9 @@
 // Explain in your own words why nestedfunction can access the variable internal.
 
 // Explanation: 
+
+
+// Nestedfucntions are located inside the function(in the function'scope). In this case, the global scope can get invoked inside and outside  of its scope. The const external can be access anywhere, however the  function nestedFunction only "lives" insde the scope of "myFunction."
 
 
 const external = "I'm outside the function";
